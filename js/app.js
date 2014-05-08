196d61633be1960872892310a8a6fe108a177c53
Alex Montas <Alexmontasmusic@gmail.com>
2014-05-07 21:52:00 -0400

    Project Completed

--- js/app.js
+++ js/app.js
@@ -9,14 +9,17 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-});
 
+  	/*--- Creates a new game ---*/
+  	$(".new").click(function(){
+  		location.reload();
+  	});
 
+//Main Application  	
 var randomNumber = Math.floor(Math.random() * 101); //random integer from 1-100
 var counter = 0; //sets up the counter variable
 var userGuess; // creates variable to insert variable from user
-
-$('#guessButton').click(function () {
+$('#guessButton').click(function () {	
 userGuess = $('#userGuess').val();  //assigns inputs from the user
 userGuess = parseInt(userGuess, 10); //ensures the user variable is an integer
 var guessTry = newGame(userGuess); //passes the user variable to the function
@@ -26,50 +29,58 @@ $("#userGuess").val('').focus();
 
 function newGame(guessTry){
 
-if ((randomNumber - guessTry) == 0)
-    {
+if (Math.abs((randomNumber - guessTry)) == 0)
+    {	
+    $('#feedback p').remove();			
     $('#feedback').append('<p>' + "You Got it!!" +'</p>');
+    prompt("You got it!!");
 	counter = 0;
+	 $('#count').html(counter);
 	}
-else if ((randomNumber - guessTry) >= 50)
+else if (Math.abs((randomNumber - guessTry)) >= 50)
 		{ 
 		  counter++;
+		   $('#feedback p').remove();	
 		   $('#feedback').append('<p>' + "You are very very Cold!!" + '</p>');
-		   $('#guessList ul').append('<li>' + guessTry + '</li>');
-		   $('#count').append(counter);
+		   $('#guessList').append('<li>' + guessTry + '</li>');
+		   $('#count').html(counter);
 		}
-else if ((randomNumber - guessTry) >= 30)
+else if (Math.abs((randomNumber - guessTry)) >= 20)
 		{
 		  counter++;
+		  $('#feedback p').remove();	
 		  $('#feedback').append('<p>' + "You are Cold!!" + '</p>');
-		  $('#guessList ul').append('<li>' + guessTry  + '</li>');
-		  $('#count').append(counter);
+		  $('#guessList').append('<li>' + guessTry  + '</li>');
+		  $('#count').html(counter);
 		}
-else if ((randomNumber - guessTry ) >= 15)
+else if (Math.abs((randomNumber - guessTry)) >= 15)
 		{
 		  counter++;
+		  $('#feedback p').remove();		
 		  $('#feedback').append('<p>' + "You are getting Warm!!" +'</p>');
-		  $('#guessList ul').append('<li>' + guessTry  + '</li>');
-		  $('#count').append(counter);
+		  $('#guessList').append('<li>' + guessTry  + '</li>');
+		  $('#count').html(counter);
 		}
-else if ((randomNumber - guessTry ) >= 10)
+else if (Math.abs((randomNumber - guessTry )) >= 7)
 		{
 		  counter++;
+		  $('#feedback p').remove();	
 		  $('#feedback').append('<p>' + "You are getting very Warm!!" +'</p>');
-		  $('#guessList ul').append('<li>' + guessTry  + '</li>');
-		  $('#count').append(counter);
+		  $('#guessList').append('<li>' + guessTry  + '</li>');
+		  $('#count').html(counter);
 		}
-else if ((randomNumber - guessTry) >= 1)
+else if (Math.abs((randomNumber - guessTry)) >= 1)
 		{
 		  counter++;
+		  $('#feedback p').remove();		
 		  $('#feedback').append('<p>' + "You are getting very very Warm!!" + '</p>');
-		  $('#guessList ul').append('<li>' + guessTry  + '</li>');
-		  $('#count').append(counter);
+		  $('#guessList').append('<li>' + guessTry  + '</li>');
+		  $('#count').html(counter);
 		} 
 
 }
 
-
+});
 
 
 
